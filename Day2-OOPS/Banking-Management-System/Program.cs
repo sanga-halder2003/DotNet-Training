4403b87341130d670c4ca946b0742022d1732ebd
SANGA HALDER <147794753+sanga-halder2003@users.noreply.github.com>
2026-06-04 01:58:38 +0530

    Add files via upload

---
+++ Day2-OOPS/Banking-Management-System/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking_Management_System
+{
+    internal class Program
+    {
+    }
+}
---
+++ Day2-OOPS/Banking-Management-System/SavingsAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking_Management_System
+{
+     class SavingsAccount :Account
+    {
+        public SavingsAccount(int accNo, string name, double bal): base(accNo, name, bal) 
+        {
+        }
+
+        public override void CalculateInterest()
+        {
+            double interest = Balance * 0.05;
+            Balance += interest;
+
+            Console.WriteLine($"\nInterest Added: {interest}");
+            Console.WriteLine($"Updated Balance: {Balance}");
+        }
+
+        // Polymorphism
+
+        public override void Withdraw(double amount)
+        {
+            if(amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds in Savings Account");
+            }
+        }
+
+
+        
+    }
+}
