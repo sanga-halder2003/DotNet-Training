66aa9f5b10705301541049cec6c45da7362f557e
SANGA HALDER <147794753+sanga-halder2003@users.noreply.github.com>
2026-06-04 21:07:12 +0530

    Add files via upload

---
+++ Day3-Handson/Mahril and Math/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mahril_and_Math
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            int target = int.Parse(Console.ReadLine());
+
+            Queue<(int num, int step)> q = new Queue<(int, int)>();
+            HashSet<int> visited = new HashSet<int>();
+
+            q.Enqueue((10, 0));
+            visited.Add(10);
+
+            while (q.Count > 0)
+            {
+                var (num, step) = q.Dequeue();
+
+                if (num == target)
+                {
+                    Console.WriteLine(step);
+                    return;
+                }
+
+                int[] next = { num + 2, num - 1, num * 3 };
+
+                foreach (int x in next)
+                {
+                    if (x >= 0 && !visited.Contains(x))
+                    {
+                        visited.Add(x);
+                        q.Enqueue((x, step + 1));
+                    }
+                }
+            }
+        }
+    }
+}
