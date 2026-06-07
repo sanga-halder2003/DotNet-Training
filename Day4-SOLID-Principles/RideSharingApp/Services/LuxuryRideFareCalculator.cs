58c60d830da3f5bce1410e7c8b05a9c62ebea3e5
SANGA HALDER <147794753+sanga-halder2003@users.noreply.github.com>
2026-06-07 13:35:44 +0530

    Update LuxuryRideFareCalculator.cs

--- Day4-SOLID-Principles/RideSharingApp/Services/LuxuryRideFareCalculator.cs
+++ Day4-SOLID-Principles/RideSharingApp/Services/LuxuryRideFareCalculator.cs
@@ -1,4 +1,7 @@
-﻿using RiderSharingApp.Interfaces;
+// OCP: Added Luxury Ride fare calculation
+// without changing NormalFareCalculator.
+
+using RiderSharingApp.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
