57f840f65c270559ccf06402356c6a3ea8840b85
SANGA HALDER <147794753+sanga-halder2003@users.noreply.github.com>
2026-06-23 01:33:29 +0530

    Add files via upload

---
+++ Day11-TaskTracker_MS/UserService/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new()
+        { new User(1,"Sanga","sangahalder@gmail.com"),
+            new User(2,"Debdyuti","debpaul@gmail.com")
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(users);
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(User user)
+        {
+            users.Add(user);
+            return Ok(users);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, User updatedUser)
+        {
+            var user = users.FirstOrDefault(t => t.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            users.Remove(user);
+            users.Add(updatedUser);
+            return Ok(updatedUser);
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+            users.Remove(user);
+            return NoContent();
+        }
+
+
+
+    }
+}
---
+++ Day11-TaskTracker_MS/UserService/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UserService.Models
+{
+    public record User
+    (
+        int Id,
+        string Name,
+        string Email
+     );
+}
